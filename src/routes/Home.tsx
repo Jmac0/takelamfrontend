1e3d70c2c6a782d548437a4d2f0ccc4a0a47957f
JamieMac <Jamiejmackenzie@gmail.com>
2022-02-16 16:30:54 +0000

    Changed splash page logo size

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,7 +5,7 @@ import logo from 'images/landing_logo.png';
 function Home() {
   return (
     <div className="home-component">
-      <img style={{ maxWidth: '40%', marginTop: '100px' }} src={logo} alt="logo" />
+      <img style={{ maxWidth: '80%', marginTop: '100px' }} src={logo} alt="logo" />
     </div>
   );
 }
