9f97876889d0c4a7a3ccb6026a16bd35dfa2decb
JamieMac <Jamiejmackenzie@gmail.com>
2022-05-13 21:55:58 +0100

    Still working on redirect from redirect

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,6 @@ export default function App() {
 			  <Route path="admin/reset" element={<ForgotPassword />} />
 			  <Route path="users/resetpassword/:token" element={<ResetPassword />} />
 
-            <Route path="/thanks" element={<ContactConfirm />} />
             <Route
               path="admin"
               element={
@@ -79,7 +78,7 @@ export default function App() {
               <Routes location={location}>
                 <Route path="/" element={<Layout path={path} />}>
                   <Route index element={<LandingPage />} />
-                  <Route path="/thanks" element={<ContactConfirm />} />
+                  <Route path="thanks" element={<ContactConfirm />} />
                   {pageContent.map((el: Component) => (
                     <Route
                       key={el._id}
